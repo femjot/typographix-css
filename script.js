8e619cf886ed799d37f28fd86532cdec5e01d32d
femjot <reactventi@email.com>
2024-09-07 17:14:03 +0200

    typographix v2 - navigation menu

--- script.js
+++ script.js
@@ -39,3 +39,18 @@ if (currentThemeFromLocalStorage) {
         lightMode();
     }
 }
+
+// Navigation Menu
+const nav = document.getElementById('nav');
+//get element without id -> css class prefixed with dot
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+    nav.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+}
+
+function hideMenu() {
+    nav.classList.remove('active');
+    menuIcon.classList.remove('active');
+}
